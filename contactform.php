843952c67d5f063574fb40ae34e421a58cfd7013
Dave Ada <davealyinovich@gmail.com>
2019-05-05 22:35:59 -0500

    latest version of website.

--- contactform.php
+++ contactform.php
@@ -5,9 +5,9 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 $formcontent="From: $firstname $lastname \nE-Mail: $email \nSubject: $subject \nMessage: $message";
-$recipient = "davealyinovich@gmail.com";
+$recipient = "jpark2284@gmail.com";
 $subject = "-----ColettePark Message-----";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "<h1 color='red' font-size='52px'>Thank You!</h1>";
+echo "<h1 color='#000' font-size='52px' font-family''goldoni', Cursive'>Thank you!  I will be in touch with you very shortly!</h1>";
 ?>
--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-	$('#mobiledropdown').hide();
-});
-
-$(document).ready(function(){
 	$('#togglebutton').click(function(){
 		$('#mobiledropdown').toggle();
 	});
